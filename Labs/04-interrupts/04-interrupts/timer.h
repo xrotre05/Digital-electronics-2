291923cf5897bcbc1263a6c6450458b898118141
unknown <xrotre05vutbr.cz>
2021-10-19 22:33:16 +0200

    04-pics

--- Labs/04-interrupts/04-interrupts/timer.h
+++ Labs/04-interrupts/04-interrupts/timer.h
@@ -61,7 +61,22 @@
  * @name  Definitions for 8-bit Timer/Counter0
  * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 8, F_CPU = 16 MHz
  */
-// WRITE YOUR CODE HERE
+	/** @brief Stop timer, prescaler 000 --> STOP */
+	#define TIM1_stop()           TCCR0B &= ~((1<<CS02) | (1<<CS01) | (1<<CS00));
+	/** @brief Set overflow 4ms, prescaler 001 --> 1 */
+	#define TIM1_overflow_4ms()   TCCR0B &= ~((1<<CS02) | (1<<CS01)); TCCR0B |= (1<<CS00);
+	/** @brief Set overflow 33ms, prescaler 010 --> 8 */
+	#define TIM1_overflow_33ms()  TCCR0B &= ~((1<<CS02) | (1<<CS00)); TCCR0B |= (1<<CS01);
+	/** @brief Set overflow 262ms, prescaler 011 --> 64 */
+	#define TIM1_overflow_262ms() TCCR0B &= ~(1<<CS02); TCCR0B |= (1<<CS01) | (1<<CS00);
+	/** @brief Set overflow 1s, prescaler 100 --> 256 */
+	#define TIM1_overflow_1s()    TCCR0B &= ~((1<<CS01) | (1<<CS00)); TCCR0B |= (1<<CS02);
+	/** @brief Set overflow 4s, prescaler // 101 --> 1024 */
+	#define TIM1_overflow_4s()    TCCR0B &= ~(1<<CS01); TCCR0B |= (1<<CS02) | (1<<CS00);
+	/** @brief Enable overflow interrupt, 1 --> enable */
+	#define TIM1_overflow_interrupt_enable()  TIMSK0 |= (1<<TOIE0);
+	/** @brief Disable overflow interrupt, 0 --> disable */
+	#define TIM1_overflow_interrupt_disable() TIMSK0 &= ~(1<<TOIE0);
 
 /**
  * @name  Definitions for 8-bit Timer/Counter2
